6bfb52b01ac9c1b95d7e759d58ffc3c21d09e7af
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2025-10-27 01:01:57 +0600

    sanity

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,20 +1,13 @@
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-// Create Sanity client
 export const client = createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: process.env.REACT_APP_SANITY_DATASET,
   apiVersion: process.env.REACT_APP_SANITY_API_VERSION,
-  token: process.env.REACT_APP_SANITY_TOKEN, 
-  useCdn: true, 
+  token: process.env.REACT_APP_SANITY_TOKEN,
+  useCdn: true,
 });
 
-// Builder for images
 const builder = imageUrlBuilder(client);
 export const urlFor = (source) => builder.image(source);
-
-// DEBUG
-console.log("Sanity projectId:", process.env.REACT_APP_SANITY_PROJECT_ID);
-console.log("Dataset:", process.env.REACT_APP_SANITY_DATASET);
-console.log("Token exists:", !!process.env.REACT_APP_SANITY_TOKEN);
